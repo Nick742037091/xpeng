1d3a1bfda1b8fc6c138d55c32c60194931c9477e
黄学聪 <huangxuecong@bluemoon.com.cn>
2024-10-17 23:49:25 +0800

    ✨ feat: 引入react-icons

--- src/app/(site)/components/TopNavigator/index.tsx
+++ src/app/(site)/components/TopNavigator/index.tsx
@@ -2,8 +2,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { AiOutlineGlobal } from 'react-icons/ai'
 
-const HomeIcon = () => {
+function HomeIcon() {
   return (
     <Link href="/" className="text-white">
       <Image
@@ -16,6 +17,28 @@ const HomeIcon = () => {
   )
 }
 
+function CenterNavigators() {
+  return <div className="flex-1"></div>
+}
+
+function RightButtons() {
+  return (
+    <div className="flex items-center gap-[24px]">
+      <Link
+        href="/test-drive"
+        className="text-white border border-white rounded-[4px] px-[16px] py-[8px] 
+        flex items-center justify-center"
+      >
+        预约试驾
+      </Link>
+      <AiOutlineGlobal style={{ color: 'white', fontSize: 24 }} />
+      <Link href="/login" className="text-white">
+        登录
+      </Link>
+    </div>
+  )
+}
+
 export default function TopNavigator() {
   // 登录页不显示顶部导航栏
   const pathname = usePathname()
@@ -25,6 +48,8 @@ export default function TopNavigator() {
   return (
     <div className="flex fixed top-0 left-0 right-0 z-50 h-[56px] items-center px-[40px]">
       <HomeIcon />
+      <CenterNavigators />
+      <RightButtons />
     </div>
   )
 }
