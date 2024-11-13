303046aea5e7a08bd0402156c5ec3b22e4f51906
Nick742037091 <742037091@qq.com>
2024-11-13 11:24:25 +0800

    🐞 fix: 调整api请求host

--- src/server/client/index.ts
+++ src/server/client/index.ts
@@ -1,8 +1,7 @@
 import { AppType } from '@/server/api'
 import { hc } from 'hono/client'
 
-const baseUrl =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL!
 
 export const client = hc<AppType>(baseUrl)
 
