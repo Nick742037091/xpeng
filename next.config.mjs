5a5b1952ae968edd7f26bb966f4895deb48e5310
Nick742037091 <742037091@qq.com>
2024-11-19 23:36:10 +0800

    ✨ feat: 禁用sentry auth token

--- next.config.mjs
+++ next.config.mjs
@@ -56,6 +56,6 @@ export default withSentryConfig(nextConfig, {
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-  authToken: process.env.SENTRY_AUTH_TOKEN
+  automaticVercelMonitors: true
+  // authToken: process.env.SENTRY_AUTH_TOKEN
 })
